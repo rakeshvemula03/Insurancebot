532415266a704f15d6e76bc9376d7e3b473454a0
rakeshvemula03 <110761440+rakeshvemula03@users.noreply.github.com>
2025-04-25 22:33:21 +0530

    Add files via upload

---
+++ Memory_for_llm.py
@@ -0,0 +1,35 @@
+from langchain_community.document_loaders import PyPDFLoader,DirectoryLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+from torch import tensor
+print("is working")
+# step 1: load raw pdfs
+Data_path = "data/"
+def load_pdf_files(data):
+    loader = DirectoryLoader(data,
+                             glob='*.pdf',
+                             loader_cls=PyPDFLoader)
+    documents =loader.load()
+    return documents
+
+documents = load_pdf_files(data = Data_path)
+#print("Length of pdf",len(documents))
+# step 2: create chunks
+def create_chunks(extracted_data):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size = 500,
+                                                   chunk_overlap=50)
+    text_chunks=text_splitter.split_documents(extracted_data)
+    return text_chunks
+text_chunks = create_chunks(extracted_data=documents)
+#print("length of text chunks",len(text_chunks))
+
+# step 3: Create vector embeddings
+def get_embedded_model():
+    embedding_model = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
+    return embedding_model
+embedding_model = get_embedded_model()
+# step 4: Store embeddings in FAISS
+DB_FAISS_PATH ="vectorstore/db_faiss"
+db=FAISS.from_documents(text_chunks,embedding_model)
+db.save_local(DB_FAISS_PATH)
